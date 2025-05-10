14dcd09e99ef37367af6a43464bf69fb86229362
annowak <anow@student.agh.edu.pl>
2025-05-10 14:41:51 +0200

    Reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'ann.nowak@agh.edu.pl'
+    const [email, setEmail] = useState('anow@agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value)
+    }
+
+    let message;
+    if (email.length < 5) {
+        message = 'Twoj email jest za krotki'
+    } else if ( email.length === 5) {
+        message = 'Twoj email jest idealny!'
+    } else {
+        message = 'Twoj email jest za dlugi!'
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}</h2>
-            <input type="text" onChange={handleChange}/>
+            <h3>{message}</h3>
+            <input type="text"  value={email} onChange={handleChange}/>
         </div>
     );
 }
