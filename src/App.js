622483864d4215dcc5cb9769fa62990b234b17dc
annowak <anow@student.agh.edu.pl>
2025-05-10 15:37:45 +0200

    Interaktywnosc

--- src/App.js
+++ src/App.js
@@ -3,29 +3,45 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-    const [email, setEmail] = useState('anow@agh.edu.pl');
+    const [email, setEmail] = useState('');
+    const [logIn, setLogIn] = useState(false)
 
     function handleChange(event) {
         setEmail(event.target.value)
     }
 
-    let message;
-    if (email.length < 5) {
-        message = 'Twoj email jest za krotki'
-    } else if ( email.length === 5) {
-        message = 'Twoj email jest idealny!'
-    } else {
-        message = 'Twoj email jest za dlugi!'
+    function changeStatus(){
+        if (logIn){
+            setLogIn(false)
+            setEmail('')
+        }else {
+            setLogIn(true)
+        }
+
     }
 
-    return (
-        <div>
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}</h2>
-            <h3>{message}</h3>
-            <input type="text"  value={email} onChange={handleChange}/>
-        </div>
-    );
+    if (!logIn) {
+        return  (
+            <div>
+                <h1>Witaj w systemie do zapisow na zajecia </h1>
+                <div>
+                    <h2>Zaloguj sie e-mailem </h2>
+                    <input type="text"  value={email} onChange={handleChange}/>
+                    <button type="button"  onClick={changeStatus}>Wchodze</button>
+                </div>
+            </div>
+        )
+    } else {
+      return (
+          <div>
+              <h2> Witaj  {email} !</h2>
+              <button type="button" onClick={changeStatus}>Wyloguj</button>
+
+          </div>
+      )
+    }
+
+
 }
 
 export default App;
